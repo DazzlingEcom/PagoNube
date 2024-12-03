a76e96bf70713ac157ef55c82df2678dc7c7f8f4
DazzlingEcom <dazzlingecom1@gmail.com>
2024-12-03 17:40:45 -0300

    Update PagoNube.py

--- PagoNube.py
+++ PagoNube.py
@@ -18,7 +18,7 @@ if uploaded_file is not None:
 
         # Leer el archivo CSV con encoding detectado
         st.write(f"Encoding detectado: {encoding}")
-        df = pd.read_csv(uploaded_file, sep=';', encoding=encoding, error_bad_lines=False)
+        df = pd.read_csv(uploaded_file, sep=';', encoding=encoding)
         st.write("Archivo leído correctamente.")
 
         # Mostrar la vista previa inicial
